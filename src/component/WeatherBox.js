839677b301b1db56c2a40186ee476aa7df1a5a56
SangJae Lee <dev.codemaestro@gmail.com>
2025-04-07 13:46:05 +0900

    feat: 화씨 추가

--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const WeatherBox = ({ weather, cityName }) => {
   const celsius = weather?.main.temp.toFixed(0);
-  // const fahrenheit = (cellsius * 1.8 + 32).toFixed(0);
+  const fahrenheit = (celsius * 1.8 + 32).toFixed(0);
   const weatherDescription = weather?.weather[0].description;
   const weatherIcon = weather?.weather[0].icon;
   const iconUrl = `http://openweathermap.org/img/wn/${weatherIcon}.png`;
@@ -19,13 +19,12 @@ const WeatherBox = ({ weather, cityName }) => {
         }}
       >
         <h3 className="m-0">
-          {celsius}
-          <span>°C</span>
+          <span>{celsius}°C</span> / <span>{fahrenheit}°F</span>
         </h3>
-        <img src={iconUrl} alt="weather icon" />
       </div>
       <div className="weather-info">
-        <h6 className="m-0">{weatherDescription}</h6>
+        <h6 className="m-0">{weatherDescription}</h6>{" "}
+        <img src={iconUrl} alt="weather icon" />
       </div>
     </div>
   );
