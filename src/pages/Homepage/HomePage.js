0f26657611b67ac3123965a4c98c84ef2b719558
SangJae Lee <dev.codemaestro@gmail.com>
2025-04-22 11:01:07 +0900

    feat : carousel

--- src/pages/Homepage/HomePage.js
+++ src/pages/Homepage/HomePage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./HomePage.style.css";
 import Banner from "./components/Banner/Banner";
+import PopularMovieSlide from "./components/PopularMovieSlide/PopularMovieSlide";
 
 const HomePage = () => {
   return (
     <div>
       <Banner />
+      <PopularMovieSlide />
     </div>
   );
 };
